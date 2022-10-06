8556c017531f1cbeeea489ce2f1fd4c79c3c47ed
KKH <kimkh7534@naver.com>
2022-10-06 15:54:21 +0900

    KKH: 메인 페이지 네비게이션 디자인 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ReactMain from './React-main';
 import TestKspark from './page/TestKspark/TestKspark';
 import Intro from './page/Intro/Intro';
 import Main from './page/Main/Main';
 
 function App() {
+  let isAuthorized = sessionStorage.getItem("isAuthorized");
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path = "/" element={<ReactMain />} />
-        <Route path = "/intro" element={<Intro />} />
-        <Route path = "/Main" element={<Main />} />
-        <Route path = "/test" element={<TestKspark />} />
+        
+        <Route path="/" element={<Main />}>
+          <Route path="/army" element={<Main />} />
+        </Route>
+        
+        <Route path="/intro" element={<Intro />} />
+
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/page/Main/Main.jsx
+++ frontend/src/page/Main/Main.jsx
@@ -1,10 +1,46 @@
 import './Main.css';
+import { AppBar, Toolbar, Typography } from "@mui/material";
+import { Outlet } from "react-router-dom";
 
 const Main = () => {
 
     return (
-        <div className="Main">
-            <div className="navigation">Connectogram</div>
+        <div className='root' style={{ flexGrow: 1 }}>
+            <AppBar position="static" style={{ alignItems: 'center', backgroundColor: '#000000' }}>
+                <Toolbar style={{width: '95%'}}>
+                    <div style={{ display: 'flex', flexDirection: 'row', left: '0%' }}>
+                        <div style={{ marginRight: '8px', fontFamily: 'Noto Sans KR' }}>전체</div>
+                        <div style={{ marginRight: '8px', fontFamily: 'Noto Sans KR' }}>육군</div>
+                        <div style={{ marginRight: '8px', fontFamily: 'Noto Sans KR' }}>해군</div>
+                        <div style={{ marginRight: '8px', fontFamily: 'Noto Sans KR' }}>공군</div>
+                        <div style={{ marginRight: '8px', fontFamily: 'Noto Sans KR' }}>해병대</div>
+                        <div style={{fontFamily: 'Noto Sans KR'}}>국직</div>
+                    </div>
+                    <Typography variant="h4" style={{ position: 'absolute', left: '50%', transform: 'translateX(-50%)', fontFamily: 'Noto Serif KR' }}>
+                        Connectogram
+                    </Typography>
+                    <div style={{ position: 'absolute', right: '0%', transform: 'translateX(-50%)', }}>
+                        사용자 정보
+                    </div>
+                </Toolbar>
+            </AppBar>
+            <AppBar position="static" style={{ alignItems: 'center', justifyContent: 'center', backgroundColor: '#ffffff', height: '50px' }}>
+                <Toolbar style={{width: '60%', display: 'flex', justifyContent: 'space-around',}}>
+                    <Typography component='div' style={{color: '#000000', fontFamily: 'Noto Sans KR'}}>
+                        DASHBOARD
+                    </Typography>
+                    <Typography component='div' style={{color: '#000000', fontFamily: 'Noto Sans KR'}}>
+                        ASSET MANAGEMENT
+                    </Typography>
+                    <Typography component='div' style={{color: '#000000', fontFamily: 'Noto Sans KR'}}>
+                        CONNECTO-MAP
+                    </Typography>
+                    <Typography component='div' style={{color: '#000000', fontFamily: 'Noto Sans KR'}}>
+                        LOG & REPORT
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Outlet />
         </div>
     );
 }
