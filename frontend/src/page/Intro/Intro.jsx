eb7533465a7a75c4289661c1eba6a7d3420f30db
KKH <kimkh7534@naver.com>
2022-10-07 19:48:49 +0900

    KKH: 로그인 페이지 디자인 완료

--- frontend/src/page/Intro/Intro.jsx
+++ frontend/src/page/Intro/Intro.jsx
@@ -4,11 +4,16 @@ import axios from "axios";
 import { useEffect } from "react";
 import Toast from '../../components/Toast/Toast';
 import PersonIcon from '@mui/icons-material/Person';
+import LockIcon from '@mui/icons-material/Lock';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 
 const Intro = () => {
     const [ToastStatus, setToastStatus] = useState(false);
     const [ToastMsg, setToastMsg] = useState("");
+
+    const [ID, setID] = useState("");
+    const [PW, setPW] = useState("");
 
     const handleToast = (msg) => {
         setToastStatus(true);
@@ -34,28 +39,38 @@ const Intro = () => {
     const excuteLogin = () => {
         axios.post("/api/session/login", {
             key: process.env.REACT_APP_APIKEY,
-            id: 'test',
-            password: 'test',
+            id: ID,
+            password: PW,
         }, { withCredentials: true }).then(res => {
             console.log(res.data);
+            if ("err_msg" in res.data) handleToast(res.data['err_msg']);
+            else handleToast("로그인 성공");
         });
-        handleToast("cancel");
     };
 
     return (
         <div className="Intro">
             <div className="subtitle">군 사이버 보안의 미래</div>
             <div className="title">Connectogram</div>
-            <div className="signin_form" style={{alignItems: 'center'}}>
-                <div className="signin_input">
-                    <PersonIcon />
+            <div className="signin_form">
+                <div className="signin_icon">
+                    <AccountCircleIcon fontSize='inherit' />
                 </div>
                 <div className="signin_input">
-
+                    <div className="signin_input_icon">
+                        <PersonIcon fontSize='medium' />
+                    </div>
+                    <div className="signin_inputbox">
+                        <input className="signin_input_com" onChange={(e) => setID(e.target.value)} placeholder="USER ID" />
+                    </div>
                 </div>
-                <div style={{}}>
-                    <div style={{borderBottom: '4px #ffffff solid', display: 'flex', alignItems: 'stretch'}}>ID <input style={{height: 'auto', backgroundColor: 'transparent', border: 0, color: '#ffffff'}} /></div>
-                    <div>PW : <input /></div>
+                <div className="signin_input">
+                    <div className="signin_input_icon">
+                        <LockIcon fontSize='medium' />
+                    </div>
+                    <div className="signin_inputbox">
+                        <input className="signin_input_com" onChange={(e) => setPW(e.target.value)} placeholder="PASSWORD" />
+                    </div>
                 </div>
             </div>
             <div className="signin_button" onClick={() => excuteLogin()}>
