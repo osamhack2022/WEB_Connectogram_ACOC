dbc96021397d60857074bc40a4d0a98663c93884
ksparkbr <ksparkbr@gmail.com>
2022-10-12 21:33:29 +0900

    세션처리(로그인/로그아웃) 방식 수정 (session_id 이용)2

--- backend/src/db/querystore/delete-session.js
+++ backend/src/db/querystore/delete-session.js
@@ -1,8 +1,8 @@
 /**
- * 세션ID로 세션테이블을 조회한다 (로그인확인용)
+ * 세션ID로 세션테이블에서 세션을 삭제한다 (로그아웃용)
  * @author 중사 박길선
  * @param {Object} param : 세션ID가 들어감(sessionID)
- * @returns {Array} session의 data를 반환한다.
+ * @returns {Array} delete 이기때문에 오류가발생하지않으면 아무값도 반환하지 않음
  * @since 2022.10.12
  */
 
