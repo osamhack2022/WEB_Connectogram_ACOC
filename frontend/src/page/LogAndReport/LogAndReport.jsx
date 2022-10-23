13d7fa5ba24e4c4fe9c25b9a751150a0e06dbc8f
KKH <kimkh7534@naver.com>
2022-10-23 16:59:51 +0900

    KKH: 위협분석 페이지 수정

--- frontend/src/page/LogAndReport/LogAndReport.jsx
+++ frontend/src/page/LogAndReport/LogAndReport.jsx
@@ -99,96 +99,102 @@ const LogAndReport = ( props ) => {
     });
 
     return (
-        <div style={{backgroundColor: 'transparent', height: '88vh' , display: 'flex', flexDirection: 'row' }}>
-            <div style={{display: 'flex', flexDirection: 'column', width: '25%', justifyContent: 'center', alignItems: 'center'}}>
-                <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '80%', height: '25vh', backgroundColor: 'rgb(0, 140, 82)', margin: '8px', borderRadius: '8px', boxShadow: '0 1px 3px 2px gray'}}>
-                    <span style={{color: '#ffffff', fontFamily: 'Noto Sans KR', fontSize: '16px'}}>Connections</span>
-                    <span style={{color: '#ffffff', fontFamily: 'Noto Sans KR', fontSize: '60px'}}>{props.LowData[0].connection.length}</span>
+        <div style={{backgroundColor: 'transparent', height: '88vh' , display: 'flex', flexDirection: 'column' }}>
+            <div style={{ height: '5vh', width: '100%', backgroundColor: 'transparent', paddingLeft: 60, paddingTop: 4, fontFamily: 'Noto Sans KR', display: 'flex', alignItems: 'flex-end'}}>
+                <span style={{ fontSize: 32}}>Client #1 위협 분석 보고서</span>
+                <span style={{ paddingLeft: 16}}>{props.LowData[0].time.split('.')[0]} 기준</span>
                 </div>
-                <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '80%', height: '25vh', backgroundColor: 'rgb(255, 92, 82)', margin: '8px', borderRadius: '8px', boxShadow: '0 1px 3px 2px gray'}}>
-                    <span style={{color: '#ffffff', fontFamily: 'Noto Sans KR', fontSize: '16px'}}>Malicious</span>
-                    <span style={{color: '#ffffff', fontFamily: 'Noto Sans KR', fontSize: '60px'}}>0</span>
+            <div style={{ display: 'flex', flexDirection: 'row'}}>
+                <div style={{display: 'flex', flexDirection: 'column', width: '25%', justifyContent: 'center', alignItems: 'center'}}>
+                    <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '80%', height: '25vh', backgroundColor: 'rgb(0, 140, 82)', margin: '8px', borderRadius: '8px', boxShadow: '0 1px 3px 2px gray'}}>
+                        <span style={{color: '#ffffff', fontFamily: 'Noto Sans KR', fontSize: '16px'}}>Connections</span>
+                        <span style={{color: '#ffffff', fontFamily: 'Noto Sans KR', fontSize: '60px'}}>{props.LowData[0].connection.length}</span>
+                    </div>
+                    <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '80%', height: '25vh', backgroundColor: 'rgb(255, 92, 82)', margin: '8px', borderRadius: '8px', boxShadow: '0 1px 3px 2px gray'}}>
+                        <span style={{color: '#ffffff', fontFamily: 'Noto Sans KR', fontSize: '16px'}}>Malicious</span>
+                        <span style={{color: '#ffffff', fontFamily: 'Noto Sans KR', fontSize: '60px'}}>0</span>
+                    </div>
+                    <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '80%', height: '25vh', backgroundColor: 'rgb(255, 171, 46)', margin: '8px', borderRadius: '8px', boxShadow: '0 1px 3px 2px gray'}}>
+                        <span style={{color: '#ffffff', fontFamily: 'Noto Sans KR', fontSize: '16px'}}>Warnings</span>
+                        <span style={{color: '#ffffff', fontFamily: 'Noto Sans KR', fontSize: '60px'}}>0</span>
+                    </div>
                 </div>
-                <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '80%', height: '25vh', backgroundColor: 'rgb(255, 171, 46)', margin: '8px', borderRadius: '8px', boxShadow: '0 1px 3px 2px gray'}}>
-                    <span style={{color: '#ffffff', fontFamily: 'Noto Sans KR', fontSize: '16px'}}>Warnings</span>
-                    <span style={{color: '#ffffff', fontFamily: 'Noto Sans KR', fontSize: '60px'}}>0</span>
-                </div>
-            </div>
-            <div style={{width: '50%', height: '100%', backgroundColor: 'transparent', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
-                <div style={{ width: '100%'}}>
-                <div style={{ height: '32px', backgroundColor: 'black', color: 'white', fontFamily: 'Noto Sans KR', paddingLeft: '8px', display: 'flex', alignItems: 'center'}}>프로토콜별 연결 수</div>
-                <div style={{ height: '30vh', backgroundColor: 'transparent', border: '1px solid black'}}>
-                    <ResponsiveTreeMap
-                        data={ProtocolData}
-                        identity="name"
-                        value="loc"
-                        valueFormat=" >-.2s"
-                        tile="binary"
-                        label="id"
-                        margin={{ top: 10, right: 10, bottom: 10, left: 10 }}
-                        labelSkipSize={12}
-                        parentLabelSize={0}
-                        labelTextColor={{
-                            from: 'color',
-                            modifiers: [
-                                [
-                                    'darker',
-                                    1.2
+                <div style={{width: '50%', height: '100%', backgroundColor: 'transparent', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
+                    <div style={{ width: '100%'}}>
+                    <div style={{ height: '4vh', backgroundColor: 'black', color: 'white', fontFamily: 'Noto Sans KR', paddingLeft: '8px', display: 'flex', alignItems: 'center'}}>프로토콜별 연결 수</div>
+                    <div style={{ height: '30vh', backgroundColor: 'transparent', border: '1px solid black'}}>
+                        <ResponsiveTreeMap
+                            data={ProtocolData}
+                            identity="name"
+                            value="loc"
+                            valueFormat=" >-.2s"
+                            tile="binary"
+                            label="id"
+                            margin={{ top: 10, right: 10, bottom: 10, left: 10 }}
+                            labelSkipSize={12}
+                            parentLabelSize={0}
+                            labelTextColor={{
+                                from: 'color',
+                                modifiers: [
+                                    [
+                                        'darker',
+                                        1.2
+                                    ]
                                 ]
-                            ]
-                        }}
-                        orientLabel={false}
-                        parentLabelTextColor={{
-                            from: 'color',
-                            modifiers: [
-                                [
-                                    'darker',
-                                    2
+                            }}
+                            orientLabel={false}
+                            parentLabelTextColor={{
+                                from: 'color',
+                                modifiers: [
+                                    [
+                                        'darker',
+                                        2
+                                    ]
                                 ]
-                            ]
-                        }}
-                        colors={{ scheme: 'nivo' }}
-                        borderColor={{
-                            from: 'color',
-                            modifiers: [
-                                [
-                                    'darker',
-                                    0.1
+                            }}
+                            colors={{ scheme: 'nivo' }}
+                            borderColor={{
+                                from: 'color',
+                                modifiers: [
+                                    [
+                                        'darker',
+                                        0.1
+                                    ]
                                 ]
-                            ]
-                        }}
-                    />
-                    
-                    
+                            }}
+                        />
+                        
+                        
+                    </div>
+                    </div>
+                    <div style={{ width: '100%', height: '44vh', backgroundColor: 'yellow', border: '1px solid black'}}></div>
                 </div>
-                </div>
-                <div style={{ width: '100%', height: '45vh', backgroundColor: 'yellow', border: '1px solid black'}}></div>
-            </div>
-            <div style={{paddingRight: '32px', paddingLeft: '32px', width: '25%', height: '100%', backgroundColor: 'transparent', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
-                <div style={{ width: '25vw', height: '40vh', border: '1px solid black'}}>
-                    <div style={{ height: '32px', backgroundColor: 'black', color: 'white', fontFamily: 'Noto Sans KR', paddingLeft: '8px', display: 'flex', alignItems: 'center'}}>국가별 연결 수</div>
-                    { !PieFlag ? <div>로딩 중...</div> : <ResponsivePie
-                        data={CountryData}   
-                        margin={{ top: 20, right: 20, bottom: 50, left: 20 }}
-                        innerRadius={0.5}
-                        padAngle={0.7}
-                        cornerRadius={3}
-                        activeOuterRadiusOffset={8}
-                        borderWidth={1}
-                        borderColor={{
-                            from: 'color',
-                            modifiers: [
-                                [
-                                    'darker',
-                                    0.2
+                <div style={{paddingRight: '32px', paddingLeft: '32px', width: '25%', height: '100%', backgroundColor: 'transparent', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
+                    <div style={{ width: '25vw', height: '39vh', border: '1px solid black', justifyContent: 'center', position: 'relative'}}>
+                        <div style={{ height: '32px', backgroundColor: 'black', color: 'white', fontFamily: 'Noto Sans KR', paddingLeft: '8px', display: 'flex', alignItems: 'center'}}>국가별 연결 수</div>
+                        { !PieFlag ? <div style={{fontFamily: 'Noto Sans KR', fontSize: 18, position: 'absolute', top: '50%', left: '50%', transform: 'translateX(-50%)'}}>로딩 중...</div> : <ResponsivePie
+                            data={CountryData}   
+                            margin={{ top: 20, right: 20, bottom: 50, left: 20 }}
+                            innerRadius={0.5}
+                            padAngle={0.7}
+                            cornerRadius={3}
+                            activeOuterRadiusOffset={8}
+                            borderWidth={1}
+                            borderColor={{
+                                from: 'color',
+                                modifiers: [
+                                    [
+                                        'darker',
+                                        0.2
+                                    ]
                                 ]
-                            ]
-                        }}
-                        arcLabel="id"
-                        enableArcLinkLabels={false}
-                    /> }
+                            }}
+                            arcLabel="id"
+                            enableArcLinkLabels={false}
+                        /> }
+                    </div>
+                    <div style={{ width: '25vw', height: '39vh', backgroundColor: 'green', border: '1px solid black'}}></div>
                 </div>
-                <div style={{ width: '25vw', height: '40vh', backgroundColor: 'green', border: '1px solid black'}}></div>
             </div>
         </div>
     );
