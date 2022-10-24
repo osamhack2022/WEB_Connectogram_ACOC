650d96eb6ab3e420a072bd3d67eb37b2fad59e75
KKH <kimkh7534@naver.com>
2022-10-24 15:54:44 +0900

    KKH: 대외기관 동향 레이아웃 수정

--- frontend/src/page/LogAndReport/LogAndReport.jsx
+++ frontend/src/page/LogAndReport/LogAndReport.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import { ResponsivePie } from '@nivo/pie';
 import { ResponsiveTreeMap } from '@nivo/treemap';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 
 
 const LogAndReport = ( props ) => {
@@ -183,7 +184,7 @@ const LogAndReport = ( props ) => {
                     </div>
                 </div>
                 <div style={{paddingRight: '32px', paddingLeft: '32px', width: '25%', height: '100%', backgroundColor: 'transparent', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
-                    <div style={{ width: '25vw', height: '39vh', border: '1px solid black', justifyContent: 'center', position: 'relative'}}>
+                    <div style={{ width: '25vw', height: '29vh', border: '1px solid black', justifyContent: 'center', position: 'relative'}}>
                         <div style={{ height: '32px', backgroundColor: 'black', color: 'white', fontFamily: 'Noto Sans KR', paddingLeft: '8px', display: 'flex', alignItems: 'center'}}>국가별 연결 수</div>
                         { !PieFlag ? <div style={{fontFamily: 'Noto Sans KR', fontSize: 18, position: 'absolute', top: '50%', left: '50%', transform: 'translateX(-50%)'}}>로딩 중...</div> : <ResponsivePie
                             data={CountryData}   
@@ -206,10 +207,88 @@ const LogAndReport = ( props ) => {
                             enableArcLinkLabels={false}
                         /> }
                     </div>
-                    <div style={{ width: '25vw', height: '39vh'}}>
+                    <div style={{ width: '25vw', height: '49vh'}}>
                         <div style={{ height: '4vh', backgroundColor: 'black', color: 'white', fontFamily: 'Noto Sans KR', paddingLeft: '8px', display: 'flex', alignItems: 'center'}}>주요 대외기관 동향</div>
-                        <div style={{ height: '35vh', backgroundColor: 'green', border: '1px solid black' }}>
-                        </div>   
+                        <div style={{ height: '45vh', backgroundColor: 'transparent', border: '1px solid black' }}>
+                            <div style={{height: '20vh', display: 'flex', flexDirection: 'row', alignItems: 'center', backgroundColor: 'transparent'}}>
+                                <div style={{width: '50%', display: 'flex', flexDirection: 'column'}}>
+                                    <div style={{width: '100%', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginBottom: '12px'}}>
+                                        <div style={{width: '60%', height: '4vh', display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+                                            <span style={{ fontFamily: 'Noto Sans KR', fontSize: '12px'}}>인터넷침해사고 경보단계</span>
+                                            <span style={{ fontFamily: 'Noto Sans KR', fontSize: '12px'}}>{} 기준</span>
+                                        </div>
+                                        <div style={{width: 0, height: 0, border: '10px solid transparent', borderRight: '10px solid rgb(255, 171, 46)' }}></div>
+                                        <div style={{ width: '20%', height: '4vh', backgroundColor:'rgb(255, 171, 46)', textAlign: 'center', lineHeight: '4vh', fontFamily: 'Noto Sans KR'}}>주의</div>
+                                    </div>
+                                    <div style={{ width: '100%', height: '11vh', display: 'flex', flexDirection: 'column', alignItems: 'center', fontFamily: "Noto Sans KR", borderTop: '1px solid black', paddingTop: '12px'}}>
+                                        <div style={{ width: '85%', justifyContent: 'center', textDecoration: 'underline'}}>오늘의 주요 키워드</div>
+                                        <div style={{ fontSize: '11px', width: '85%', height: '9.8vh', overflow: 'hidden', wordWrap: 'break-word', display: '-webkit-box', WebkitLineClamp: 4, WebkitBoxOrient: 'vertical', textOverflow: 'ellipsis'}}>
+                                            vCenter Server , VMware , ESXi , iOS , Apple , Android , 안드로이드 , Apache Commons Text , Oracle , E-Business Suite , Zoom , Dimension , Commerce , Acrobat , ColdFusion
+                                        </div>
+                                    </div>
+                                </div>
+                                <div style={{ width: '50%', height: '20vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', borderLeft: '1px solid black'}}>
+                                    <div style={{ width: '100%', textAlign: 'start', paddingBottom: '16px', paddingLeft: '16px', fontFamily: 'Noto Sans KR'}}>
+                                        <span style={{textDecoration: 'underline'}}>오늘의 사이버 위협</span> <span style={{fontSize: '8px'}}>(2022.10.24)</span></div>
+                                    <div style={{width: '93%'}}>
+                                        <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', fontFamily: 'Noto Sans KR'}}>
+                                            <div style={{ width: '74%', backgroundColor: '#ffff22', textAlign: 'center', fontSize: '14px', padding: '6px'}}>악성코드 발견 홈페이지</div>
+                                            <div style={{ width: '26%', backgroundColor: 'white', display: 'flex', flexDirection: 'row', fontSize: '12px', padding: '6px', alignItems: 'center', justifyContent: 'center'}}>
+                                                <div>50(개)</div>
+                                                <ArrowDropUpIcon sx={{color: 'red'}} fontSize="small" />
+                                            </div>
+                                        </div>
+                                        <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', fontFamily: 'Noto Sans KR'}}>
+                                            <div style={{ width: '74%', backgroundColor: '#ffff22', textAlign: 'center', fontSize: '14px', padding: '6px'}}>신종 스미싱 악성 앱</div>
+                                            <div style={{ width: '26%', backgroundColor: 'white', display: 'flex', flexDirection: 'row', fontSize: '12px', padding: '6px', alignItems: 'center', justifyContent: 'center'}}>
+                                                <div>50(개)</div>
+                                                <ArrowDropUpIcon sx={{color: 'red'}} fontSize="small" />
+                                            </div>
+                                        </div>
+                                        <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', fontFamily: 'Noto Sans KR'}}>
+                                            <div style={{ width: '74%', backgroundColor: '#ffff22', textAlign: 'center', fontSize: '14px', padding: '6px'}}>피싱ㆍ파밍 차단 사이트</div>
+                                            <div style={{ width: '26%', backgroundColor: 'white', display: 'flex', flexDirection: 'row', fontSize: '12px', padding: '6px', alignItems: 'center', justifyContent: 'center'}}>
+                                                <div>50(개)</div>
+                                                <ArrowDropUpIcon sx={{color: 'red'}} fontSize="small" />
+                                            </div>
+                                        </div> 
+                                    </div>                              
+                                </div>
+                            </div>
+                            <div style={{width: '100%', height: '25vh', borderTop: '1px solid black', fontFamily: 'Noto Sans KR'}}>
+                                <div style={{textDecoration: 'underline', marginTop: '12px', marginLeft: '12px'}}>최신 자료</div>
+                                <div style={{width: '100%', height: '4vh', display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                                    <div style={{width: '75%', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap', paddingLeft: '12px', paddingRight: '32px'}}>
+                                        카카오 서비스 장애 이슈를 악용한 사이버 공격에 대한 주의 권고
+                                    </div>
+                                    <div style={{width: '25%', fontSize: '12px', textAlign: 'center'}}>2022.10.21</div>
+                                </div>
+                                <div style={{width: '100%', height: '4vh', display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                                    <div style={{width: '75%', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap', paddingLeft: '12px', paddingRight: '32px'}}>
+                                        카카오 서비스 장애 이슈를 악용한 사이버 공격에 대한 주의 권고
+                                    </div>
+                                    <div style={{width: '25%', fontSize: '12px', textAlign: 'center'}}>2022.10.21</div>
+                                </div>
+                                <div style={{width: '100%', height: '4vh', display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                                    <div style={{width: '75%', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap', paddingLeft: '12px', paddingRight: '32px'}}>
+                                        카카오 서비스 장애 이슈를 악용한 사이버 공격에 대한 주의 권고
+                                    </div>
+                                    <div style={{width: '25%', fontSize: '12px', textAlign: 'center'}}>2022.10.21</div>
+                                </div>
+                                <div style={{width: '100%', height: '4vh', display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                                    <div style={{width: '75%', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap', paddingLeft: '12px', paddingRight: '32px'}}>
+                                        카카오 서비스 장애 이슈를 악용한 사이버 공격에 대한 주의 권고
+                                    </div>
+                                    <div style={{width: '25%', fontSize: '12px', textAlign: 'center'}}>2022.10.21</div>
+                                </div>
+                                <div style={{width: '100%', height: '4vh', display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                                    <div style={{width: '75%', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap', paddingLeft: '12px', paddingRight: '32px'}}>
+                                        카카오 서비스 장애 이슈를 악용한 사이버 공격에 대한 주의 권고
+                                    </div>
+                                    <div style={{width: '25%', fontSize: '12px', textAlign: 'center'}}>2022.10.21</div>
+                                </div>
+                            </div>
+                        </div> 
                     </div>
                 </div>
             </div>
